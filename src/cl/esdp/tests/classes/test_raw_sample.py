54f148f9e15da79c38ae84be929791f7606daf8a
Alexander Sokol <sokol@compatibl.com>
2022-08-02 00:14:06 -0400

    Added RawSample and test.

---
+++ src/cl/esdp/tests/classes/test_raw_sample.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2003-present CompatibL. All rights reserved.
+#
+# This file contains valuable trade secrets and may be copied, stored, used,
+# or distributed only in compliance with the terms of a written commercial
+# license from CompatibL and with the inclusion of this copyright notice.
+
+import pytest
+
+from cl.esdp.core.classes.raw_sample import RawSample
+
+
+def test_compare():
+    """Test RawSample comparison."""
+
+    # Two empty instances are not equal
+    assert RawSample() != RawSample()
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])
