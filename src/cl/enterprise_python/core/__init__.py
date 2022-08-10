b35ebeecab8798f114718776ddc973502f80b12a
Alexander Sokol <sokol@compatibl.com>
2022-08-09 20:53:17 -0400

    Added __init__.py for core.

---
+++ src/cl/enterprise_python/core/__init__.py
@@ -0,0 +1,3 @@
+from cl.enterprise_python.core.classes.unsafe_class import UnsafeClass
+from cl.enterprise_python.core.classes.slots_class import SlotsClass
+from cl.enterprise_python.core.classes.attrs_class import AttrsClass
