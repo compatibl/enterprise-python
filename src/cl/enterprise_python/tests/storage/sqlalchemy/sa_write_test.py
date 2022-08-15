9d3aa9438dce19a181849fe8b902e5fa002bc036
Alexander Sokol <sokol@compatibl.com>
2022-08-14 22:42:02 -0400

    Basic sqlalchemy test.

--- src/cl/enterprise_python/tests/storage/sqlalchemy/sa_write_test.py
+++ src/cl/enterprise_python/tests/storage/sqlalchemy/sa_write_test.py
@@ -14,10 +14,7 @@
 
 import pytest
 import sqlalchemy as sa
-
-import cl.enterprise_python.core as ep
 from sqlalchemy.orm import Session
-from approvaltests import verify
 from cl.enterprise_python.mocks.storage.sqlalchemy.sa_simple_record_mock import SaSimpleRecordMock
 
 
@@ -30,19 +27,15 @@ class SaSimpleRecordTest:
         """Test writing."""
 
         from sqlalchemy import create_engine
-        engine = create_engine("sqlite:///test.db", echo=True, future=True)
+        engine = create_engine("sqlite://", echo=True, future=True)
 
         connection = engine.connect()
         metadata = sa.MetaData()
-
         table = sa.Table('simple_record_mock', metadata,
-                       sa.Column('simple_id', sa.Integer()),
-                       sa.Column('string_element', sa.String(255), nullable=False),
-                       )
-
+                         sa.Column('simple_id', sa.Integer()),
+                         sa.Column('string_element', sa.String(255), nullable=False),
+                         )
         metadata.create_all(engine)  # Creates the table
-
-        from sqlalchemy.orm import Session
 
         with Session(engine) as session:
             obj_1 = SaSimpleRecordMock(simple_id="A", string_element="AA")
