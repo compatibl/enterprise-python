4b63ae1bc10db79da55d2dec712ec2fc2c528284
Alexander Sokol <sokol@compatibl.com>
2022-08-14 23:05:25 -0400

    Delete test db file before and after test.

--- src/cl/enterprise_python/tests/storage/sqlalchemy/sa_write_test.py
+++ src/cl/enterprise_python/tests/storage/sqlalchemy/sa_write_test.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import pytest
+import os
 import sqlalchemy as sa
 from sqlalchemy.orm import Session
 from cl.enterprise_python.mocks.storage.sqlalchemy.sa_simple_record_mock import SaSimpleRecordMock
@@ -26,21 +27,32 @@ class SaSimpleRecordTest:
     def test_write(self):
         """Test writing."""
 
-        from sqlalchemy import create_engine
-        engine = create_engine("sqlite://", echo=True, future=True)
+        file_name_db = "sa_write_test.db"
 
-        connection = engine.connect()
-        metadata = sa.MetaData()
-        table = sa.Table('simple_record_mock', metadata,
-                         sa.Column('simple_id', sa.Integer()),
-                         sa.Column('string_element', sa.String(255), nullable=False),
-                         )
-        metadata.create_all(engine)  # Creates the table
+        # Remove DB file before test, in case it was left over from previous run
+        if os.path.exists(file_name_db):
+            os.remove(file_name_db)
 
-        with Session(engine) as session:
-            obj_1 = SaSimpleRecordMock(simple_id="A", string_element="AA")
-            session.add_all([obj_1])
-            session.commit()
+        engine = sa.create_engine(f"sqlite:///{file_name_db}", echo=True, future=True)
+
+        with engine.connect() as connection:
+
+            metadata = sa.MetaData()
+            table = sa.Table('simple_record_mock', metadata,
+                             sa.Column('simple_id', sa.Integer()),
+                             sa.Column('string_element', sa.String(255), nullable=False),
+                             )
+            metadata.create_all(engine)  # Creates the table
+
+            with Session(engine) as session:
+                obj_1 = SaSimpleRecordMock(simple_id="A", string_element="AA")
+                session.add_all([obj_1])
+                session.commit()
+
+        if False:
+            # Remove DB file after test
+            if os.path.exists(file_name_db):
+                os.remove(file_name_db)
 
 
 if __name__ == "__main__":
