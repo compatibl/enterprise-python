173f3f44d63267a7212b4b983dd7a3b386534164
Alexander Sokol <sokol@compatibl.com>
2022-08-15 08:14:12 -0400

    Created DeepSwap using MongoEngine ODM and deep style of embedding.

---
+++ src/cl/enterprise_python/core/schema/deep/deep_leg.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2021-present CompatibL
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from cl.enterprise_python.core.schema.deep.deep_swap_key import DeepSwapKey
+
+
+class DeepLeg:
+    """Swap leg."""
+
+    leg_type: str = None
+    """Leg type."""
+
+    leg_ccy: str = None
+    """Leg currency."""
