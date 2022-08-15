5dd9983dec923a737bd661d5d4bf2f2eef28f719
Alexander Sokol <sokol@compatibl.com>
2022-08-15 10:51:29 -0400

    Stub for relational schema still using MongoEngine.

---
+++ src/cl/enterprise_python/core/schema/rel/rel_leg.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2021-present CompatibL
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import mongoengine as me
+
+
+class RelLeg(me.EmbeddedDocument):  # Must inherit from EmbeddedDocument rather than Document
+    """Swap leg."""
+
+    leg_type = me.StringField(max_length=50)
+    """Leg type."""
+
+    leg_ccy = me.StringField(max_length=3)
+    """Leg currency."""
