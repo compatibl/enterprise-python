0f6ad46389556b279950d38852c8c68c55a06d10
Alexander Sokol <sokol@compatibl.com>
2022-08-17 05:55:22 -0400

    Added clear and query.

--- src/cl/enterprise_python/core/api/fastapi/fastapi_blotter.py
+++ src/cl/enterprise_python/core/api/fastapi/fastapi_blotter.py
@@ -13,12 +13,15 @@
 # limitations under the License.
 
 import mongoengine as me
-from typing import List
+from typing import List, Dict, Optional
 import uvicorn
 from fastapi import FastAPI, HTTPException
 from cl.enterprise_python.core.schema.tree.tree_leg import TreeLeg
 from cl.enterprise_python.core.schema.tree.tree_swap import TreeSwap
 from cl.enterprise_python.core.schema.tree.tree_trade import TreeTrade
+
+# Use connection alias specified in 'meta' attribute of the data types for the test
+connection_alias = "tree"
 
 app = FastAPI()
 
@@ -48,19 +51,16 @@ def create_trades(trade_count: int) -> List[TreeTrade]:
 
 
 @app.get("/")
-def get_root():
+def get_root() -> str:
     """This result is returned when you access the main URL."""
     return "Welcome to FastAPI Trade Blotter!"
 
 
 @app.get("/add_trades/{trade_count}")
-def add_trades(trade_count: int):
+def add_trades(trade_count: int) -> str:
     """
     Create and add to DB the specified number of random swap records.
     """
-
-    # Use connection alias specified in 'meta' attribute of the data types for the test
-    connection_alias = "tree"
 
     # Connect to the database using test-specific alias
     connection = me.connect(connection_alias, alias=connection_alias)
@@ -75,15 +75,42 @@ def add_trades(trade_count: int):
     return f"Success: Added {trade_count} trades. DB now has {total_count} trades."
 
 
+@app.get("/get_trade/{trade_id}")
+def get_trade(trade_id: str) -> Dict[str, str]:
+    """
+    Retrieve from DB by trade_id and return the specified trade.
+    """
+    trade = TreeTrade.objects(trade_id=trade_id).one()
+    return trade.to_json()
+
+
 @app.get("/clear")
-def clear_trades():
-    """Clear all trades."""
-    return "Done"
+def clear_trades() -> str:
+    """Clear all trades from the database."""
+
+    # Connect to the database using test-specific alias
+    connection = me.connect(connection_alias, alias=connection_alias)
+
+    # Count the current number of trades
+    total_count = TreeTrade.objects.count()
+
+    connection.drop_database(connection_alias)
+    return f"Success: {total_count} trades cleared from DB."
 
 
 @app.get("/book_trades")
-def read_item():
-    trades = create_records()
+def query_trades(leg_ccy: Optional[str] = None) -> Dict[str, str]:
+    """
+    If leg_ccy is specified, return all trades where the currency for
+    at least one of the legs is leg_ccy, otherwise return all trades.
+    """
+
+    # If leg_ccy is specified, return all trades where the currency for
+    # at least one of the legs is leg_ccy, otherwise return all trades.
+    if leg_ccy is not None:
+        trades = TreeSwap.objects(legs__leg_ccy=leg_ccy).order_by('trade_id')
+    else:
+        trades = TreeSwap.objects.order_by('trade_id')
     result = {"trades": [trade.to_json() for trade in trades]}
     return result
 
