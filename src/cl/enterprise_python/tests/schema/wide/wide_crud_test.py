77db0d54e8dffd35a751772596e165d23f6b84f9
Alexander Sokol <sokol@compatibl.com>
2022-08-24 15:22:13 -0400

    Eliminated linter warning using type annotation, improved comments.

--- src/cl/enterprise_python/tests/schema/wide/wide_crud_test.py
+++ src/cl/enterprise_python/tests/schema/wide/wide_crud_test.py
@@ -59,7 +59,7 @@ class WideCrudTest:
         ccy_count = len(ccy_list)
 
         # Create swap records
-        swaps = [
+        swaps: List[WideTrade] = [
             WideSwap(
                 trade_id=f"T{i+1}",
                 trade_type="Swap",
@@ -70,7 +70,7 @@ class WideCrudTest:
             )
             for i in range(0, 2)
         ]
-        bonds = [
+        bonds: List[WideTrade] = [
             WideBond(
                 trade_id=f"T{i+1}",
                 trade_type="Bond",
@@ -78,6 +78,7 @@ class WideCrudTest:
             )
             for i in range(2, 3)
         ]
+
         return swaps + bonds
 
     def test_crud(self):
@@ -96,21 +97,37 @@ class WideCrudTest:
         # Retrieve all trades ordered by trade_id to avoid
         # receiving records in random order
         all_trades = WideTrade.objects.order_by('trade_id')
+
+        # Add the result to approvaltests file
         result += "All Trades:\n" + "".join(
             [f"    trade_id={trade.trade_id} trade_type={trade.trade_type}\n" for trade in all_trades]
         )
 
         # Retrieve only those trades that have type WideSwap
         all_swaps = WideSwap.objects.order_by('trade_id')
+
+        # Add the result to approvaltests file
         result += "All Swaps:\n" + "".join(
             [f"    trade_id={trade.trade_id} trade_type={trade.trade_type}\n" for trade in all_swaps]
         )
 
-        # Retrieve only those trades that have type WideSwap
-        # and have GBP as fixed leg currency
+        # The objective is to retrieve only those trades that have type WideSwap
+        # and have GBP currency for the fixed leg. Having GBP currency for the
+        # floating leg does not count.
+
+        # This Iterable includes trades where leg 1 has type=Fixed and ccy=GBP
         gbp_fixed_leg_1_swaps = WideSwap.objects(leg_type_1="Fixed", leg_ccy_1="GBP").order_by('trade_id')
+
+        # This Iterable includes trades where leg 2 has type=Fixed and ccy=GBP
         gbp_fixed_leg_2_swaps = WideSwap.objects(leg_type_2="Fixed", leg_ccy_2="GBP").order_by('trade_id')
+
+        # This list combines items in both Iterables. For the purposes of this
+        # exercise, we will assume that each swap has one Fixed leg and one
+        # Floating leg (most of the swaps traded in the market are like that),
+        # so we do not need to eliminate duplicates.
         gbp_fixed = list(gbp_fixed_leg_1_swaps) + list(gbp_fixed_leg_2_swaps)
+
+        # Add the result to approvaltests file
         result += "Swaps where fixed leg has GBP currency:\n" + "".join(
             [f"    trade_id={trade.trade_id} trade_type={trade.trade_type} "
              f"leg_type_1={trade.leg_type_1} leg_ccy_1={trade.leg_ccy_1} "
